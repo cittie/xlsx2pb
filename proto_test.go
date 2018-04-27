7dacc1a7104df1136cbd0ac9b91c8692757793aa
Jiangyi <jiangyi@taiyouxi.cn>
2018-04-27 17:30:37 +0800

    Update testcase

--- proto_test.go
+++ proto_test.go
@@ -45,30 +45,36 @@ func genTestProtoRow() *ProtoRow {
 
 func TestAddMessage(t *testing.T) {
 	pr := genTestProtoRow()
+	var idx int
+
+	checkOutput := func(out string) {
+		assert.Equal(t, out, pr.outProto[idx])
+		idx++
+	}
 
 	pr.AddMessageHead(pr.Name)
-	assert.Equal(t, "message TestProtoRow {", pr.outProto[0])
+	checkOutput("message TestProtoRow {")
 
 	pr.AddVal(pr.vars[0])
-	assert.Equal(t, "  /* * This is TestFiled1 * */", pr.outProto[1])
-	assert.Equal(t, "  string TestField1 = 1;", pr.outProto[2])
+	checkOutput("  /* * This is TestFiled1 * */")
+	checkOutput("  string TestField1 = 1;")
 
 	pr.AddRepeat(pr.repeats[0])
-	assert.Equal(t, "  ", pr.outProto[3])
-	assert.Equal(t, "  message TestRepeatStruct {", pr.outProto[4])
-	assert.Equal(t, "    /* ** This is TestRepeat1 ** */", pr.outProto[5])
-	assert.Equal(t, "    int64 TestRepeat1 = 1;", pr.outProto[6])
-	assert.Equal(t, "  }", pr.outProto[7])
-	assert.Equal(t, "  ", pr.outProto[8])
-	assert.Equal(t, "  repeated TestRepeatStruct testRepeatStruct = 2;", pr.outProto[9])
+	checkOutput("  ")
+	checkOutput("  message TestRepeatStruct {")
+	checkOutput("    /* ** This is TestRepeat1 ** */")
+	checkOutput("    int64 TestRepeat1 = 1;")
+	checkOutput("  }")
+	checkOutput("  ")
+	checkOutput("  repeated TestRepeatStruct testRepeatStruct = 2;")
 
 	pr.AddMessageTail()
-	assert.Equal(t, "}", pr.outProto[10])
-	assert.Equal(t, "", pr.outProto[11])
+	checkOutput("}")
+	checkOutput("")
 
 	pr.AddMessageArray()
-	assert.Equal(t, "message TestProtoRow_ARRAY {", pr.outProto[12])
-	assert.Equal(t, "  repeated TestProtoRow testProtoRow = 1;", pr.outProto[13])
-	assert.Equal(t, "}", pr.outProto[14])
-	assert.Equal(t, "", pr.outProto[15])
+	checkOutput("message TestProtoRow_ARRAY {")
+	checkOutput("  repeated TestProtoRow testProtoRow = 1;")
+	checkOutput("}")
+	checkOutput("")
 }
