43ba6778a14072eba7e81a5129a557953b0cfac3
JiangYi <jiangyi@taiyouxi.cn>
2018-06-04 20:01:46 +0800

    Add main

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"flag"
+	"github.com/cittie/xlsx2pb/lib"
+)
+
+func main() {
+	var useCache = flag.Bool("cache", true, "Use cache for current xlsx")
+
+	lib.Run(*useCache)
+}
